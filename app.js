c810fb943ce2a8f0375e5cba49df77822dcfa254
msaimsohail <116447889+msaimsohail@users.noreply.github.com>
2022-10-31 03:19:00 -0700

    hello world

---
+++ app.js
@@ -0,0 +1,66 @@
+alert("Having Assignment");
+// window.alert("Thanks for your input");
+
+
+// var a = "Saim";
+// alert(a);
+
+
+//  var a = 30;
+//  var b = 20;
+//  var c = a + b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a - b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a * b;
+// alert(c);
+
+// var a = 30;
+//  var b = 20;
+//  var c = a / b;
+// alert(c);
+
+// var a = 30;
+//  var b = 10;
+//  var c = 5;
+//  var d = (a + b) - c;
+// alert(d);
+
+
+// var a = 23;
+// var c = a + 7;
+// alert(c);
+
+
+// var a = 50;
+// var b = 40;
+// a++;
+// b--;
+// console.log(a);
+// console.log(b);
+
+
+// var a = 10;
+// var b = ++a;
+// alert(b);
+// alert(a);
+
+
+// var alpha = 1;
+// var beta =  3;
+// var gamma = 4;
+// var total = (alpha + beta) * gamma
+// alert(total); 
+
+
+// var message = "HI";
+// var namee = "Saim";
+// var specificword = '!';
+// var add = message + namee + specificword;
+// alert(add);
